549af9c118e79cf27025beb93cb811496262b0d5
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-10-05 17:45:59 -0700

    Update backend.tf

--- aws_la_cloudplayground_multiple_workers_version/backend.tf
+++ aws_la_cloudplayground_multiple_workers_version/backend.tf
@@ -1,5 +1,7 @@
 #Set S3 backend for persisting TF state file remotely, ensure bucket already exits
 # And that AWS user being used by TF has read/write perms
+#s3 backend stores terraform state in an s3 bucket to avoid teams stepping on each other's toes and uses Dynamodb for consistency checking and state locking
+#when you run terraform init, terraform will prompt you to transfer existing state files to the new s3 bucket. 
 terraform {
   required_version = ">=0.12.0"
   required_providers {
@@ -12,3 +14,4 @@ terraform {
     bucket  = "<name-of-already-created-bucket>"
   }
 }
+
